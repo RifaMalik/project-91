eefeb1b2fcba225d5cd754ca9e09408902ac0df0
RifaMalik <96971487+RifaMalik@users.noreply.github.com>
2022-10-09 19:16:51 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -14,14 +14,14 @@ document.getElementById("player_question").innerHTML="Question Turn - " + player
 document.getElementById("player_answer").innerHTML="Answer Turn - " + player2_name;
 
 function Send() {
-    number1 = document.getElementById("number_1").value;
-    number2 = document.getElementById("number_2").value;
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
     actual_answer=parseInt(number1) * parseInt(number2);
     question_word ="<h4 id='word_display'>Q."+number1+"X" + number2+ "</h4>";
     input_box= "<br>Answer : <input type='text' id='input_check_box'>";
     check_button="<br><br><button class='btn btn-info' onclick='check()'>CHECK</button>";
     row=question_word+input_box+check_button;
     document.getElementById("output").innerHTML=row;
-    document.getElementById("number_1").value="";
-    document.getElementById("number_2").value="";
+    document.getElementById("number1").value="";
+    document.getElementById("number2").value="";
 }
